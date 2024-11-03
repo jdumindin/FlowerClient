618d090f8f6007e1f06fc342ede3d585030ccb34
jdumindin <jndumindin@gmail.com>
2024-11-02 22:07:32 -0700

    11/2/24

---
+++ src/app/continent/continent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-continent',
+  standalone: true,
+  imports: [],
+  templateUrl: './continent.component.html',
+  styleUrl: './continent.component.css'
+})
+export class ContinentComponent {
+
+}
---
+++ src/app/species/species.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-species',
+  standalone: true,
+  imports: [],
+  templateUrl: './species.component.html',
+  styleUrl: './species.component.css'
+})
+export class SpeciesComponent {
+
+}
